a02365a8ca59e0be49e6dd3e130bc99d560c8ff9
gfreitas15 <gfmonguilhott793@gmail.com>
2025-10-30 13:36:42 -0300

    Adicionado entidades Cliente/Restaurante/Produto/Pedido

---
+++ src/main/java/com/deliverytech/delivery_api/entity/Produto.java
@@ -0,0 +1,88 @@
+package com.deliverytech.delivery_api.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String nome;
+
+	@Column(nullable = false)
+	private BigDecimal preco;
+
+	@Column(nullable = false)
+	private String categoria;
+
+	@Column(nullable = false)
+	private boolean disponivel = true;
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "restaurante_id", nullable = false)
+	private Restaurante restaurante;
+
+	public Produto() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public boolean isDisponivel() {
+		return disponivel;
+	}
+
+	public void setDisponivel(boolean disponivel) {
+		this.disponivel = disponivel;
+	}
+
+	public Restaurante getRestaurante() {
+		return restaurante;
+	}
+
+	public void setRestaurante(Restaurante restaurante) {
+		this.restaurante = restaurante;
+	}
+}
+
+
---
+++ src/main/java/com/deliverytech/delivery_api/entity/Restaurante.java
@@ -0,0 +1,74 @@
+package com.deliverytech.delivery_api.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "restaurantes")
+public class Restaurante {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String nome;
+
+	@Column(nullable = false)
+	private String categoria;
+
+	@Column(nullable = false)
+	private boolean ativo = true;
+
+	@Column(precision = 3, scale = 2)
+	private BigDecimal avaliacao; // 0.00 a 5.00
+
+	public Restaurante() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public BigDecimal getAvaliacao() {
+		return avaliacao;
+	}
+
+	public void setAvaliacao(BigDecimal avaliacao) {
+		this.avaliacao = avaliacao;
+	}
+}
+
+
